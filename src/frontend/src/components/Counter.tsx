9c25a7fb82d68942735aa1e055437bb7779a7512
Brandon Chin <chitangb.chin@gmail.com>
2024-10-08 16:36:00 -0400

    Initial commit

---
+++ src/frontend/src/components/Counter.tsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react";
+
+import Spinner from "./Spinner";
+import { useBackend } from "../ic/Actors";
+import { useInternetIdentity } from "ic-use-internet-identity";
+
+export function Counter() {
+  const { actor: backend } = useBackend();
+  const { identity } = useInternetIdentity();
+  const [loading, setLoading] = useState<boolean>(true);
+  const [counter, setCounter] = useState<number>();
+
+  // Get counter value from backend on mount
+  useEffect(() => {
+    if (!backend) return;
+    backend.get_counter().then((c) => {
+      setCounter(c);
+      setLoading(false);
+    });
+  }, [backend]);
+
+  // Add 1 to counter on click
+  function handleClick() {
+    if (!backend) return;
+    setLoading(true);
+    backend.inc_counter().then((c) => {
+      setCounter(c);
+      setLoading(false);
+    });
+  }
+
+  let buttonClassName =
+    "inline-block h-8 md:h-16 px-5 ml-2 font-bold text-white bg-blue-500 rounded disabled:bg-blue-500/20 disabled:hover:bg-blue-500/20";
+  if (loading) {
+    buttonClassName += " cursor-wait";
+  } else {
+    buttonClassName += " cursor-pointer hover:bg-blue-700";
+  }
+
+  if (!identity) return null;
+
+  return (
+    <div className="flex flex-col items-center gap-5 md:flex-row">
+      Counter:
+      <div className="inline-block h-8 px-5 ml-3 rounded md:h-16 bg-zinc-600">
+        {counter === undefined ? (
+          <Spinner className="w-4 h-8 md:w-10 md:h-16" />
+        ) : (
+          counter
+        )}
+      </div>
+      <button
+        onClick={handleClick}
+        className={buttonClassName}
+        disabled={loading}
+      >
+        {loading ? <Spinner className="w-4 h-8 md:w-10 md:h-16" /> : "+"}
+      </button>
+    </div>
+  );
+}
---
+++ src/frontend/src/components/LoginButton.tsx
@@ -0,0 +1,42 @@
+import Spinner from "./Spinner";
+import { twMerge } from "tailwind-merge";
+import { useInternetIdentity } from "ic-use-internet-identity";
+
+export function LoginButton() {
+  const { isLoggingIn, login, clear, identity } = useInternetIdentity();
+
+  // If the user is logged in, clear the identity. Otherwise, log in.
+  function handleClick() {
+    if (identity) {
+      clear();
+    } else {
+      login();
+    }
+  }
+
+  let className =
+    "flex px-5 font-bold text-white bg-blue-500 rounded cursor-pointer h-9 md:h-16 hover:bg-blue-700 disabled:bg-blue-500/20 disabled:hover:bg-blue-500/20";
+  className = isLoggingIn
+    ? twMerge(className, "cursor-wait")
+    : twMerge(className, "cursor-pointer");
+
+  const text = () => {
+    if (identity) {
+      return "Logout";
+    } else if (isLoggingIn) {
+      return (
+        <>
+          Logging in
+          <Spinner className="w-10 h-10 ml-3 md:w-20 md:h-20" />
+        </>
+      );
+    }
+    return "Login";
+  };
+
+  return (
+    <button onClick={handleClick} className={className} disabled={isLoggingIn}>
+      {text()}
+    </button>
+  );
+}
